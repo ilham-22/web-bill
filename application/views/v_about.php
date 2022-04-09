050c608945fe7b220db60dac54fa4f2336e1ea01
ilham220500 <ilham220500@gmail.com>
2022-04-10 01:37:07 +0700

    web bill

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : ilham</li>
+ <li>Nama Belakang : pangeran</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan ancur no 1</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>germany</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>biliard</li>
+ <li>dugem</li>
+ </ul>
+</section>
